cc0f654926bfdecf546a66fef34b0fad86b4a05b
Balázs Oláh <balazsolah19@gmail.com>
2023-09-30 17:05:43 +0200

    small changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -15,10 +16,34 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          fontFamily: GoogleFonts.inter().fontFamily),
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text('Blizz Chat'),
+        ),
+        body: const Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Icon(
+              Icons.chat,
+              size: 200,
+            ),
+            Text(
+              'Blizz Chat',
+              style: TextStyle(fontSize: 72, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              'End-to-end instant messaging for everyone',
+              style: TextStyle(fontSize: 32),
+              textAlign: TextAlign.center,
+            ),
+          ],
         ),
       ),
     );
