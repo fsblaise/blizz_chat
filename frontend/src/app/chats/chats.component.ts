8bf20c117ddfc8da608f4ddaea5b27fed8659fb8
Balázs Oláh <balazsolah19@gmail.com>
2024-05-10 22:48:10 +0200

    Implemented stories page. Adjusted text fields and active chat recognition.

--- frontend/src/app/chats/chats.component.ts
+++ frontend/src/app/chats/chats.component.ts
@@ -6,7 +6,7 @@ import { MatInput } from '@angular/material/input';
 import { MatIcon } from '@angular/material/icon';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { FormsModule } from '@angular/forms';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink, RouterOutlet } from '@angular/router';
 import { TextFieldComponent } from '../../shared/components/text-field/text-field.component';
 
 @Component({
@@ -18,6 +18,13 @@ import { TextFieldComponent } from '../../shared/components/text-field/text-fiel
 })
 export class ChatsComponent {
   searchWord!: string;
-  active = 0;
-  chats: string[] = ['Test User1', 'Test User2', 'Test User3', 'Test User4', 'Test User5', 'Test User6']
+  active = -1;
+  chats: string[] = ['Test User1', 'Test User2', 'Test User3', 'Test User4', 'Test User5', 'Test User6'];
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    const url = this.router.url.split('/').pop();
+    const decoded = decodeURIComponent(url as string);
+    this.active = this.chats.includes(decoded) ? this.chats.findIndex(value => value === decoded) : -1;
+  }
 }
--- frontend/src/app/stories/stories.component.ts
+++ frontend/src/app/stories/stories.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-stories',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatIcon],
   templateUrl: './stories.component.html',
   styleUrl: './stories.component.css'
 })
 export class StoriesComponent {
-
+  stories: string[] = ['Test User','Test User','Test User','Test User','Test User','Test User','Test User','Test User','Test User','Test User','Test User','Test User','Test User','Test User','Test User','Test User','Test User','Test User'];
 }
