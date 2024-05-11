ca847c63f6f56675f8efed96f2885d2a1d5204ea
Balázs Oláh <balazsolah19@gmail.com>
2024-05-11 23:14:07 +0200

    Leaflet added to the stack. Implemented map page with client rendered map layer.

--- frontend/src/app/map/map.component.ts
+++ frontend/src/app/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { afterNextRender, Component } from '@angular/core';
 
 @Component({
   selector: 'app-map',
@@ -8,5 +8,43 @@ import { Component } from '@angular/core';
   styleUrl: './map.component.css'
 })
 export class MapComponent {
+  map: any;
+  L: any = null;
 
+  constructor() {
+    afterNextRender(async () => {
+      this.L = await import('leaflet');
+      this.setupMap();
+    })
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  async setupMap(): Promise<void> {
+    if (this.L) {
+      this.map = this.L.map('map').setView([0,0], 13);
+
+      // Add a tilelayer
+      this.L.tileLayer(
+        'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+      ).addTo(this.map);
+
+      this.getCurrentLocation();
+    }
+
+  }
+
+  getCurrentLocation() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        console.log('asd');
+        this.map.setView([position.coords.latitude, position.coords.longitude]);
+      },
+      error => {
+        // alert("Geolocation is not supported by this browser.");
+        console.error(error);
+      });
+  }
 }
